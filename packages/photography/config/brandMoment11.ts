24b30196b10f7f0745b5ecff4b7f3eba60737fa7
Dominik Piatek <dominik@meetcleo.com>
2025-07-25 17:54:43 +0100

    Add brandMoment11 landscape large for fake door BNPL

--- packages/photography/config/brandMoment11.ts
+++ packages/photography/config/brandMoment11.ts
@@ -1,7 +1,7 @@
 import { CropType, PhotographyConfig } from '../types/types';
 
 const config: PhotographyConfig = {
-  crops: [CropType.PORTRAIT_LARGE, CropType.PORTRAIT_MEDIUM],
+  crops: [CropType.PORTRAIT_LARGE, CropType.PORTRAIT_MEDIUM, CropType.LANDSCAPE_LARGE],
   focalPoint: {
     originalImage: {
       dimensions: {
