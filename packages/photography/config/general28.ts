08f5e12ef1ffb024c944a92eb7d90f478e43f98b
KirstMorgans <44839352+kmorgans1@users.noreply.github.com>
2026-02-17 10:33:01 +0000

    update crop for portrait

--- packages/photography/config/general28.ts
+++ packages/photography/config/general28.ts
@@ -1,6 +1,34 @@
-import {  PhotographyConfig } from '../types/types';
+import { PhotographyConfig } from '../types/types';
 
-  const config: PhotographyConfig = {
-  };
+const config: PhotographyConfig = {
+  focalPoint: {
+    originalImage: {
+      dimensions: {
+        width: 3028,
+        height: 3001,
+      },
+      focalPoint: {
+        x: 2060,
+        y: 1260,
+        normalizedX: 0.68,
+        normalizedY: 0.42,
+      },
+    },
+    crops: {
+      PORTRAIT_LARGE: {
+        extractParams: {
+          top: 1150,
+          left: 330,
+          width: 1918,
+          height: 1900,
+        },
+        targetDimensions: {
+          width: 375,
+          height: 812,
+        },
+      },
+    },
+  },
+};
 
-  export default config;
+export default config;
